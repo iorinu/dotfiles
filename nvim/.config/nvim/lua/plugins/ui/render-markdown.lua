e06473c92c59ce6208b60e387e33c78d1b204ded
iorimacbook <iorinu@users.noreply.github.com>
2026-05-07 00:05:08 +0900

    add desc nvim render-markdown

--- nvim/.config/nvim/lua/plugins/ui/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/ui/render-markdown.lua
@@ -1,8 +1,11 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-	keys = {
-		{ "<Space>md", ":RenderMarkdown toggle<CR>", desc = "Toggle Render Markdown" },
-	},
-	opts = {},
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  ft = { "markdown", "mdx" },
+  keys = {
+    { "<Space>md", ":RenderMarkdown toggle<CR>", desc = "Toggle Render Markdown" },
+  },
+  opts = {
+    render_modes = true,
+  },
 }
